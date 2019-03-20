bf1e34800ce29273a37850538a4b49e988ecd48b
denglang <1099263878@qq.com>
2019-03-20 21:37:06 +0800

    斧头扔出, 完成一半

--- game-server/app/service/Action.ts
+++ game-server/app/service/Action.ts
@@ -8,7 +8,7 @@ export default class Action {
 
     seatId: number = 0;             // 谁的动作
     cmd = -1;                       // 动作的类型
-    data: {turn: number, speed: number} = null;               // 动作的描述
+    data: any = null;               // 动作的描述
 
     setSeatId(seatId: number) {
         this.seatId = seatId;
